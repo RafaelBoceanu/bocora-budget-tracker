5399e4765ba6b1b8b30519068fa7127f71b36d32
RafaelBoceanu <rafaelboceanu@gmail.com>
2026-03-30 11:27:20 +0100

    Bug fix #3

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,6 @@ export default defineConfig({
         theme_color: '#166534',
         background_color: '#ffffff',
         display: 'standalone',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
-        ],
       },
     }),
   ],
