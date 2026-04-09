08e1003d39fd86b008db19ed96efbad0574523ee
RafaelBoceanu <rafaelboceanu@gmail.com>
2026-04-09 13:46:26 +0100

    Restore project files

---
+++ app/src/main/java/app/vercel/backpacker_budget_tracker/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.vercel.backpacker_budget_tracker.twa;
+
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
