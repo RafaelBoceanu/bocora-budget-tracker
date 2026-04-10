20ed4165a49009aa1c006ba9f9dbfc3130e45333
RafaelBoceanu <rafaelboceanu@gmail.com>
2026-04-10 12:07:25 +0100

    Updated DelegationService

--- app/src/main/java/app/bocora/twa/DelegationService.java
+++ app/src/main/java/app/bocora/twa/DelegationService.java
@@ -1,18 +1,6 @@
 package app.bocora.twa;
 
+import com.google.androidbrowserhelper.trusted.DelegationService;
 
-import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
-
-
-public class DelegationService extends
-        com.google.androidbrowserhelper.trusted.DelegationService {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        
-            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
-        
-    }
+public class DelegationService extends com.google.androidbrowserhelper.trusted.DelegationService {
 }
-
